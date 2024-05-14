a66c6cddd8a69113f63afa4362772a240e6ec052
Earthbert <daraban_timotei@tutanota.com>
2024-05-15 01:58:06 +0300

    Add function fitInterval function
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

--- server/src/sensor.cpp
+++ server/src/sensor.cpp
@@ -12,6 +12,17 @@ SimpleDHT11 dht11(D1);
 byte temperature = 30;
 byte humidity = 100;
 
+static int fitInterval(int value, int in_min, int in_max, int out_min, int out_max) {
+	if (value < in_min) {
+		return out_min;
+	}
+	if (value > in_max) {
+		return out_max;
+	}
+	float slope = 1.0 * (out_max - out_min) / (in_max - in_min);
+	return out_min + slope * (value - in_min);
+}
+
 void readSensors() {
 	auto ret = dht11.read(&temperature, &humidity, NULL);
 	if (ret != SimpleDHTErrSuccess) {
@@ -34,7 +45,7 @@ void readSensors() {
 		digitalWrite(HUMIDITY_LED, LOW);
 	}
 
-	int motorSpeed = map(temperature, motorInterval.lower, motorInterval.upper, 0, 1023);
+	int motorSpeed = fitInterval(temperature, motorInterval.lower, motorInterval.upper, 0, 1023);
 	Serial.printf("Motor speed: %d\n", motorSpeed);
 	turnOnMotor(motorSpeed);
 }
